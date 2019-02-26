1f68b529a2b5a2031a7f482ebd10045a83220dff
Tipping Bottles <haileyroberts23331@gmail.com>
2019-02-25 18:41:05 -0700

    Haileys'Activities
    are finished.

---
+++ Serenity/Serenity/Controllers/SerenityController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Serenity.Controllers
+{
+    public class SerenityController : Controller
+    {
+        // GET: Serenity
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		public ActionResult NameAnimalsAlphabetically()
+		{
+			return View();
+		}
+
+		public ActionResult QuestionYourThoughts()
+		{
+			return View();
+		}
+
+	}
+}
