f97aac67934cf5631be9d045ab38019d25927cf2
Brandyn <bbushong@student.neumont.edu>
2019-02-26 18:06:15 -0700

    committing so i can pull

---
+++ Serenity/Serenity/Models/ActivityModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Activities.Models
+{
+	public class ActivityModel
+	{
+		public string ScriptUrl { get; set; }
+		public String ActivityName { get; set; }
+		public int ActivityId { get; set; }
+		public int Raiting { get; set; }
+	}
+}
