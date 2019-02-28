1d931c1649c0808d6c5261360337515a4bf2a99f
Tipping Bottles <haileyroberts23331@gmail.com>
2019-02-27 18:30:04 -0700

    suhavaif

--- Serenity/Serenity/Models/ActivityModel.cs
+++ Serenity/Serenity/Models/ActivityModel.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace Activities.Models
+namespace Serenity.Models
 {
 	public class ActivityModel
 	{
 		public string ScriptUrl { get; set; }
 		public String ActivityName { get; set; }
 		public int ActivityId { get; set; }
-		public int Raiting { get; set; }
+		public int Rating { get; set; }
 	}
 }
