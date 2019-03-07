895089ff35d30f1aea79d60f9318eae8cd75b265
Brandyn <bbushong@student.neumont.edu>
2019-03-06 17:11:24 -0700

    music ready to verify

--- Serenity/Serenity/Scripts/play.js
+++ Serenity/Serenity/Scripts/play.js
@@ -1,6 +1,6 @@
 var audio = document.getElementById('audio');
 var currentSong = 0;
-var songs = ['music/supermario.mp3', 'music/happytogether.mp3'];
+var songs = ['/Content/music/bensound-love.mp3', '/Content/music/bensound-memories.mp3', '/Content/music/bensound-slowmotion.mp3', '/Content/music/bensound-tenderness.mp3', '/Content/music/bensound-thelounge.mp3', '/Content/music/bensound-tomorrow.mp3', '/Content/music/bensound-betterdays.mp3'];
 
 function loadPlaylist() {
     if (currentSong == songs.length) {
