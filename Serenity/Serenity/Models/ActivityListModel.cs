2ec242782ada14307d41a90fb80b8c03c6413ad4
Brandyn <bbushong@student.neumont.edu>
2019-03-07 17:08:08 -0700

    commit so i can pull

--- Serenity/Serenity/Models/ActivityListModel.cs
+++ Serenity/Serenity/Models/ActivityListModel.cs
@@ -8,5 +8,10 @@ namespace Serenity.Models
     public class ActivityListModel
     {
         public List<ActivityModel> Activities { get; set; }
+
+        public ActivityListModel(List<ActivityModel> activities)
+        {
+            Activities = activities;
+        }
     }
 }
---
+++ Serenity/Serenity/Services/EFActivityProduct.cs
@@ -0,0 +1,22 @@
+﻿using Serenity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Serenity.Services
+{
+    public class EFActivityProduct
+    {
+        public ActivityListModel GetAllActivities()
+        {
+            List<ActivityModel> activities = new List<ActivityModel>();
+            ActivityListModel activityList = new ActivityListModel(activities);
+
+            using(var context = new SerenityDatabaseEntities())
+            {
+                
+            }
+        }
+    }
+}
